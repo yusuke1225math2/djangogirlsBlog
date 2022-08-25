ba35f0a93bd85960bbf1e717a977079072b3c87f
yusuke1225math2 <tambo@saiand.co.jp>
2022-08-25 13:36:50 +0900

    Change route settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', 'localhost', '.herokuapp.com']
 
 
 # Application definition
